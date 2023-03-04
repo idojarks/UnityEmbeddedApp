deacae0d7ee3b918382030f8d0655d35645ba2f7
idojarks <idojarks@gmail.com>
2023-03-04 10:27:58 +0900

    change namespace

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Windows;
 
-namespace WpfApp1
+namespace DreamKit
 {
   /// <summary>
   /// Interaction logic for MainWindow.xaml
--- WpfApp1/UnityWindow/UnityWindow.cs
+++ WpfApp1/UnityWindow/UnityWindow.cs
@@ -9,7 +9,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WpfApp1
+namespace DreamKit
 {
   internal sealed class UnityWindow
   {
